d7e7bb98e6e9399dd268a63cb1fea6b182e97a8d
DenGrif <grifon991@gmail.com>
2024-06-03 23:02:07 +0400

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,12 @@
+first_name = 'Vasya'
+apple_count = 10
+second_name = 'Petya'
+orange_count = 15
+
+print(first_name, 'дал',second_name, apple_count,'яблок и', orange_count)
+
+Number_of_completed = 12
+Number_of_hours = 1.5
+Course_name = 'Python,'
+Time_per_task = (Number_of_hours // Number_of_hours)
+print('Курс:', Course_name, 'всего задач: 12, затрачено часов: 1.5, среднее время выполнения', Number_of_hours/ Number_of_completed)
